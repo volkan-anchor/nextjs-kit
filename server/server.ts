7dbe26854859ed46629ec713603fd75a362e4139
Luis Gonçalves <luis.goncalves@anchorlabs.com>
2022-04-07 16:59:59 +0000

    Code review

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,10 @@
-const express = require('express')
-const next = require('next')
-const proxy = require('http-proxy-middleware');
-const { GoogleAuth } = require('google-auth-library');
-const execSync = require('child_process').execSync;
-const fs = require('fs');
+import express, { NextFunction, Request, Response } from "express";
+import next from 'next';
+import { GoogleAuth, IdTokenClient } from 'google-auth-library';
+import { execSync } from "child_process";
+import fs from "fs";
 
-
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = parseInt(process.env.PORT!, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -14,10 +12,10 @@ const handle = app.getRequestHandler()
 const DEFAULT_TARGET_AUDIENCE =
   '541620653883-djvsk0b5pv2s7m006mmdvq1a9si8h1n2.apps.googleusercontent.com';
 
-function rawBodyMiddleware(req, res, next) {
+function rawBodyMiddleware(req: any, res: Response, next: NextFunction) {
   req.setEncoding('utf8');
   req.rawBody = '';
-  req.on('data', function (chunk) {
+  req.on('data', function (chunk: any) {
     req.rawBody += chunk;
   });
   req.on('end', function () {
@@ -29,8 +27,8 @@ function rawBodyMiddleware(req, res, next) {
   });
 }
 
-function iapProxy(client, url, target) {
-  return async function (req, res, next) {
+function iapProxy(client: IdTokenClient, url: string, target: string) {
+  return async function (req: any, res: Response, next: NextFunction) {
     if (req.url !== url) {
       return next();
     }
@@ -50,7 +48,7 @@ function iapProxy(client, url, target) {
         timeout: 60000,
       });
       return res.status(200).send(response.data);
-    } catch (error) {
+    } catch (error: any) {
       const code = error.code;
       const data = error.response.data;
       return res.status(code).send(data);
@@ -66,11 +64,11 @@ app.prepare().then(async () => {
   const server = express()
 
   const keyLocation =
-  process.env.HOME + '/.config/dotfiles/frontend-tester-key.json';
+    process.env.HOME + '/.config/dotfiles/frontend-tester-key.json';
   if (!fs.existsSync(keyLocation)) {
     execSync(
       'gcloud iam service-accounts keys create --iam-account frontend-tester@development-204920.iam.gserviceaccount.com ' +
-        keyLocation
+      keyLocation
     );
   }
 
@@ -82,12 +80,11 @@ app.prepare().then(async () => {
   server.use(iapProxy(client, '/v1', TARGET));
   server.use(iapProxy(client, '/graphql', TARGET));
 
-  server.all('*', (req, res) => {
+  server.all("*", (req: Request, res: Response) => {
     return handle(req, res)
   })
 
-  server.listen(port, (err) => {
-    if (err) throw err
+  server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`)
   })
 })
